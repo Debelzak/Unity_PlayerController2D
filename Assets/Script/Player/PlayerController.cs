5303f90cf4964b3bf884cae84d4f8d942ecb83b2
Debelzak <debelzak26@gmail.com>
2021-02-04 03:04:58 -0300

    Slope implementation

--- Assets/Script/Player/PlayerController.cs
+++ Assets/Script/Player/PlayerController.cs
@@ -8,13 +8,23 @@ public class PlayerController : MonoBehaviour
     private CapsuleCollider2D playerCollider;
     public LayerMask collisionMask;
     public GameObject groundCheck;
-    public float groundCheckRadius;
+    public PhysicsMaterial2D fullfriction;
+    public PhysicsMaterial2D nofriction;
 
+    public float groundCheckRadius;
     public float moveSpeed;
     public float jumpForce;
-
+    public bool canJump;
+    public bool isJumping;
     private float moveAmountX;
     private float moveAmountY;
+
+    public float slopeCheckDistance;
+    private float slopeSideAngle;
+    private float slopeDownAngle;
+    private float slopeDownAngleOld;
+    private Vector2 slopeNormalPerpendicular;
+    public bool isOnSlope;
 
     private void Start()
     {
@@ -30,6 +40,8 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate() 
     {
         HandleInput();
+        GroundCheck();
+        SlopeCheck();
         HandleMovement();
     }
 
@@ -38,12 +50,24 @@ public class PlayerController : MonoBehaviour
     {
         moveAmountX = rigidBody.velocity.x;
         moveAmountY = rigidBody.velocity.y;
+
         jump = false;
 
-        isGrounded = GroundCheck();
+        if(isGrounded && !isOnSlope && !isJumping)
+        {
+            moveAmountX = moveInputX * moveSpeed;
+            moveAmountY = 0;
+        }
+        else if(isGrounded && isOnSlope && !isJumping)
+        {
+            moveAmountX = moveSpeed * slopeNormalPerpendicular.x * -moveInputX;
+            moveAmountY = moveSpeed * slopeNormalPerpendicular.y * -moveInputX;
+        }
+        else if(!isGrounded)
+        {
+            moveAmountX = moveInputX * moveSpeed;
+        }
 
-        //Move
-        moveAmountX = moveInputX * moveSpeed;
         //Jump
         if(isGrounded && jumpInput)
         {
@@ -60,37 +84,88 @@ public class PlayerController : MonoBehaviour
 
     void Move(float velocityX, float velocityY, bool jump)
     {
-        if(jump)
+        if(canJump && jump)
         {
-            velocityY += jumpForce;
+            velocityY = jumpForce;
+            isJumping = true;
+            canJump = false;
         }
-     
+
         rigidBody.velocity = new Vector2(velocityX, velocityY);
     }
 
     public bool isGrounded;
-    bool GroundCheck()
+    void GroundCheck()
     {
-        if(Physics2D.OverlapCircle(groundCheck.transform.position, groundCheckRadius, collisionMask))
+        isGrounded = Physics2D.OverlapCircle(groundCheck.transform.position, groundCheckRadius, collisionMask);
+
+        if(rigidBody.velocity.y <= 0)
         {
-            return true;
+            isJumping = false;
         }
-        return false;
+
+        if(isGrounded && !isJumping)
+        {
+            canJump = true;
+        }
     }
 
     void SlopeCheck()
     {  
-        Vector2 CheckPos = transform.position - new Vector3(0, playerCollider.size.y / 2);
+        Vector2 checkPos = transform.position - new Vector3(0, playerCollider.size.y / 2);
+        SlopeCheckVertical(checkPos);
+        //SlopeCheckHorizontal(checkPos);
     }
 
-    void SlopeCheckHorizontal()
+    void SlopeCheckHorizontal(Vector2 checkPos)
     {
+        RaycastHit2D slopeHitFront = Physics2D.Raycast(checkPos, transform.right, slopeCheckDistance, collisionMask);
+        RaycastHit2D slopeHitBack = Physics2D.Raycast(checkPos, -transform.right, slopeCheckDistance, collisionMask);
 
-    }
+        if(slopeHitFront)
+        {
+            isOnSlope = true;
+            slopeSideAngle = Vector2.Angle(slopeHitFront.normal, Vector2.up);
+        }
+        else if(slopeHitBack)
+        {
+            isOnSlope = true;
+            slopeSideAngle = Vector2.Angle(slopeHitBack.normal, Vector2.up);
+        }
+        else
+        {
+            slopeSideAngle = 0;
+            isOnSlope = false;
+        }
+    }  
 
-    void SlopeCheckVertical()
+    void SlopeCheckVertical(Vector2 checkPos)
     {
-        
+        RaycastHit2D hit = Physics2D.Raycast(checkPos, Vector2.down, slopeCheckDistance, collisionMask);
+        if(hit)
+        {
+            slopeNormalPerpendicular = Vector2.Perpendicular(hit.normal).normalized;
+            slopeDownAngle = Vector2.Angle(hit.normal, Vector2.up);
+
+            if(slopeDownAngle != slopeDownAngleOld)
+            {
+                isOnSlope = true;
+            }
+            
+            slopeDownAngleOld = slopeDownAngle;
+
+            Debug.DrawRay(hit.point, hit.normal, Color.red);
+            Debug.DrawRay(hit.point, slopeNormalPerpendicular, Color.red);
+        }
+
+        if(isOnSlope && moveInputX == 0)
+        {
+            rigidBody.sharedMaterial = fullfriction;
+        }
+        else
+        {
+            rigidBody.sharedMaterial = nofriction;
+        }
     }
 
     float moveInputX;
